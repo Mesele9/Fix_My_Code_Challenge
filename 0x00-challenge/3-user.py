d9efd211fd42fb87dd79b70031456bc8e61a5142
Mesele Assefa <getmesy@gmail.com>
2023-02-24 23:38:10 -0800

    correct 3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -21,7 +21,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
-        self.__password = None
+
 
     @property
     def password(self):
@@ -38,7 +38,7 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or not isinstance(pwd, str):
+        if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
@@ -51,9 +51,11 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
-        if pwd is None or not isinstance(pwd, str):
+        if pwd is None or type(pwd) is not str:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        if self.__password is None:
+            return False
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
