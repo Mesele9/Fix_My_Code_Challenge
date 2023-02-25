bb18cf031d4af91e435bf3dcaa9535be2bab8b5c
Mesele Assefa <getmesy@gmail.com>
2023-02-24 23:07:31 -0800

    correction

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -21,6 +21,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
+        self.__password = None
 
     @property
     def password(self):
@@ -51,8 +52,6 @@ class User():
         - Compare `__password` and the MD5 value of `pwd`
         """
         if pwd is None or not isinstance(pwd, str):
-            return False
-        if self.__password is None:
             return False
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
